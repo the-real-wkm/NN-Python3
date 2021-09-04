742b0e1036b0f8d69334141a24a1993eb61f42e9
William M <the.real.wkm@gmail.com>
2021-09-04 15:22:35 -0500

    Add Format Method of String formatting

--- lessons/string_format.py
+++ lessons/string_format.py
@@ -4,3 +4,8 @@ num2 = 10.2903948
 # PREVIOUS
 #print('#1 is', num1, 'and #2 is', num2)
 
+# FORMAT METHOD
+# You can use :.3 to assign a precision of 3 to the numbers (i.e. 3.14 / 10.2)
+# You can also use :.3f to assign a precision of 3 spaces following the decimal to the numbers
+
+print('#1 is {0:.3f} and #2 is {1:.3f}'.format(num1, num2))
