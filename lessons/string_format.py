81674a41e1975f79531c873d3a8b2632ba39886a
William M <the.real.wkm@gmail.com>
2021-09-04 15:26:10 -0500

    Add Regions / Add F-Strings example

--- lessons/string_format.py
+++ lessons/string_format.py
@@ -1,11 +1,24 @@
 num1 = 3.1425467389
 num2 = 10.2903948
 
-# PREVIOUS
-#print('#1 is', num1, 'and #2 is', num2)
 
-# FORMAT METHOD
+#region PREVIOUS
+
+# print('#1 is', num1, 'and #2 is', num2)
+
+#endregion
+
+#region FORMAT METHOD
+
 # You can use :.3 to assign a precision of 3 to the numbers (i.e. 3.14 / 10.2)
 # You can also use :.3f to assign a precision of 3 spaces following the decimal to the numbers
 
-print('#1 is {0:.3f} and #2 is {1:.3f}'.format(num1, num2))
+# print('#1 is {0:.3f} and #2 is {1:.3f}'.format(num1, num2))
+
+#endregion
+
+#region USING F-STRINGS
+
+print(f'#1 is {num1:.4f} and #2 is {num2:.4f}')
+
+#endregion
