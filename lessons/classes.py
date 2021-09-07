c29cead80e44ba4601926c46836eeb0e3dec7bb0
William M <the.real.wkm@gmail.com>
2021-09-07 18:58:02 -0500

    Planet Class and examples of what cane be done with classes
    init method
    
    Class Methods
    
    Static Methods
    
    objects, etc.

---
+++ lessons/classes.py
@@ -0,0 +1,44 @@
+
+class Planet:
+
+    shape = 'Spherical'
+
+    @classmethod
+    def commons(cls):
+        return f'Planets are a {cls.shape} shape due to gravity.'
+
+    @staticmethod
+    def spin(speed = '[Speed Unknown]'):
+        return f'The Planet spins at a speed of: {speed}'
+
+    # The init funtion is similar to a constructor
+    def __init__(self, name, radius="Unknown", gravity="Unknown", system="Unknown"):
+        self.name = name
+        self.radius = radius
+        self.gravity = gravity
+        self.system = system
+
+    def orbit(self):
+        return(f'{self.name} is orbiting in the {self.system}')
+
+hoth = Planet('Hoth', 200000, 5.5, 'Hoth System')
+naboo = Planet('Naboo', 300000, 8, 'Naboo System')
+
+print(f'''\nName: {hoth.name}
+Radius: {hoth.radius}
+Gravity: {hoth.gravity}
+
+{hoth.orbit()}
+
+-----
+
+Name: {naboo.name}
+Radius: {naboo.radius}
+Gravity: {naboo.gravity}
+
+{naboo.orbit()}
+
+-----
+
+{Planet.commons()}
+{Planet.spin(2000)}\n''')
