782a363ee1e22b56683c01ce0f23ba47230eced8
William M <the.real.wkm@gmail.com>
2021-09-07 16:53:11 -0500

    Range Examples

--- lessons/ranges.py
+++ lessons/ranges.py
@@ -1,2 +1,31 @@
 
-things = [1, 2, 3, 4, 5, 6]
+#region Range Number Examples
+
+# 14 = Exclusive upperbound
+# for n in range(14):
+#     print(n)
+
+# 6 = Inclusive lowerbound
+# 14 = Exclusive upperbound
+# for n in range(6, 14):
+#    print(n)
+
+# 0 = Inclusive lowerbound
+# 667 = Exclusive upperbound
+# 18 = Step (Amount increased each increment)
+#for n in range(0, 667, 18):
+#    print(n)
+
+#endregion
+
+strains = ['Maui Wowie', 'Watermelon Zkittlez', 'Pineapple Express', 'Purple Punch', 'Sour Tangie', 'Blue Dream']
+
+# Based on the length of strains, print its items in a formatted numbered list
+# for n in range(len(strains)):
+#     print(f'{n+1}) {strains[n]}')
+
+# Based on the length of strains, starts at the end of the list and working backwards prints the numbered list in reverse.
+# for n in range(len(strains) - 1, -1, -1):
+#     print(f'{n+1}) {strains[n]}')
+
+
