d31877f626f894a761cdf7138a23985bd86a39ba
William M <the.real.wkm@gmail.com>
2021-09-07 16:15:56 -0500

    Create Loops.py and 2 for loop examples
    First for loop example prints a formatted list of all users from hard-coded list.
    
    Second for loop example prints a section of the same list from index one, up to but not including index 4.

---
+++ lessons/loops.py
@@ -0,0 +1,15 @@
+
+users = [ 'Foxy', 'RedWolf', 'Zeeker', 'Kyleptomaniac', 'SkullKid' ]
+
+# region Print Users For-Loop
+# print('\nUSERS:\n')
+
+# for user in users:
+#     print(f'\t{user}')
+
+# print('\n')
+
+# for user in users[1:4]:
+#     print(user)
+
+
